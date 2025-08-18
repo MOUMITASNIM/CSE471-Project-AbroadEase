d8f11cb3902e8a68ed20230bf176749afa0054ff
Tasnim Rahman Moumita <tasnimr025@gmail.com>
2025-08-18 13:54:02 +0600

    Add files via upload

--- apps/api/models/Post.js
+++ apps/api/models/Post.js
@@ -5,7 +5,15 @@ const postSchema = new Schema({
   body: String,
   type: { type: String, enum: ['SOP', 'VISA'] },
   country: String,
+  // SOP specific fields
+  university: String,
+  program: String,
+  department: String,
+  term: String,
+  degreeType: { type: String, enum: ['Bachelors', 'Masters', 'PhD', 'Diploma', 'Certificate'] },
+  // Common fields
   tags: [String],
   author: { type: Schema.Types.ObjectId, ref: 'User' }
 }, { timestamps: true });
+
 export default mongoose.model('Post', postSchema);
